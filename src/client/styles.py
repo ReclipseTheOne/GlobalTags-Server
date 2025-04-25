c312d7da222c79b4020c04435e6ca3d78b916f86
Reclipse <17797013+ReclipseTheOne@users.noreply.github.com>
2025-04-25 14:43:21 +0300

    CSS progress

--- src/client/styles.py
+++ src/client/styles.py
@@ -1,12 +1,66 @@
+class Colors:
+    BACKGROUND = "#180C1C"
+    BACKGROUND_RGB = "24, 12, 28"
+    SECONDARY = "#EDAE88"
+    THIRD = "#FFD2C2"
+
+
 class Styles:
-    WINDOW_BAR_BUTTON = """
-        QPushButton {
+    WINDOW_BAR_BUTTON = f"""
+        QPushButton {{
             background-color: transparent;
             border: none;
             padding: 5px;
             font-size: 16px;
-        }
-        QPushButton:hover {
+        }}
+        QPushButton:hover {{
             background-color: rgba(0, 0, 0, 0.1);
-        }
+        }}
+    """
+
+    STATUS_WIDGET = f"""
+        background-color: {Colors.BACKGROUND};
+        border-radius: 5px;
+    """
+
+    STATUS_LABEL = f"""
+        font-size: 18px;
+        font-weight: bold;
+        color: #d9534f;
+    """
+
+    STATS_WIDGET = f"""
+        color: {Colors.THIRD};
+        background-color: {Colors.BACKGROUND};
+        border-radius: 5px;
+    """
+
+    IMAGE_BUTTON = f"""
+        QPushButton {{
+            border: none;
+            background-color:
+            transparent;
+        }}
+    """
+
+    MAIN_WIDGET = f"""
+        background-color: {Colors.BACKGROUND};
+    """
+
+    TITLE_BAR = f"""
+        font-size: 14px;
+        font-weight: bold;
+        color: {Colors.SECONDARY};
+        background-color: transparent;
+    """
+
+    STATS_LABEL = f"""
+        color: {Colors.SECONDARY};
+        font-size: 12px;
+    """
+
+    STATS_VALUE = f"""
+        color: {Colors.THIRD};
+        font-size: 16px;
+        font-weight: bold;
     """
